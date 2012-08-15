3c9b5962539dba07f5d35cfe1c38b7cb2941a3c7
Juan F. Pérez <j.guitar@gmail.com>
2012-08-15 19:26:30 +0200

    unit 5, task as a class

--- unit5/Task.js
+++ unit5/Task.js
@@ -1,3 +1,24 @@
 var Task = function(taskText){
+  var done = false;
+  var text = taskText;
 
+  this.toString = function(){
+    return 'Task: ' + text + '. Done: ' + done;
+  }
+
+  this.do = function(){
+    this.done = true;
+  };
+
+  this.undo = function(){
+    this.done = false;
+  };
+
+  this.isDone = function(){
+    return this.done;
+  };
+
+  this.hasText = function(aText){
+    return this.text === aText;
+  };
 };
