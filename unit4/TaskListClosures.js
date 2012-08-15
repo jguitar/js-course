739659a339e4c9b9108bd26c30c1137472d43e1d
Juan F. Pérez <j.guitar@gmail.com>
2012-08-15 18:34:53 +0200

    unit 4 green

--- unit4/TaskListClosures.js
+++ unit4/TaskListClosures.js
@@ -1,10 +1,56 @@
-
 var taskList = [];
 
 var add = function(text){
-
+  task = {
+    text: text,
+    done: false
+  }
+  taskList.push(task);
 }
 
 var remove = function(text){
+  for (var i = 0; i < taskList.length; i++){
+    if (taskList[i].text == text){
+      taskList.splice(i, 1);
+      return true;
+    }
+  }
+}
 
+var doTask = function(text){
+  for (var i = 0; i < taskList.length; i++){
+    if (taskList[i].text == text){
+      taskList[i].done = true;
+    }
+  }
+}
+
+var doneList = function(){
+  result = [];
+  for (var i = 0; i < taskList.length; i++){
+    if (taskList[i].done) {
+      result.push(taskList[i]);
+    }
+  }
+  return result;
+}
+
+var pendingList = function(){
+  result = [];
+  for (var i = 0; i < taskList.length; i++){
+    if (!taskList[i].done) {
+      result.push(taskList[i]);
+    }
+  }
+  return result;
+}
+
+var all = function(){
+  return taskList;
+}
+
+var toConsole = function(){
+  for (var i = 0; i < taskList.length; i++){
+    console.log('Task #', i + 1 + ':', taskList[i].text, '. Done:', taskList[i].done);
+  }
 }
--- unit4/jasmine-1.2.0/spec/TaskListClosuresSpec.js
+++ unit4/jasmine-1.2.0/spec/TaskListClosuresSpec.js
@@ -1,4 +1,51 @@
 describe("TaskListClosures", function() {
-  it('starts', function(){
+  it('adds a new task', function(){
+    add('a task');
+    expect(taskList.length).toEqual(1);
   });
+
+  it('removes a valid task', function(){
+    remove('a task');
+    expect(taskList.length).toEqual(0);
+  });
+
+  it('do a valid task', function(){
+    add('a task');
+    expect(taskList[0].done).toEqual(false);
+    doTask('a task');
+    expect(taskList[0].done).toEqual(true);
+  });
+
+  it('returns a done list', function(){
+    // hack to empty all
+    remove('a task');
+
+    add('a task');
+    expect(doneList().length).toEqual(0);
+    doTask('a task');
+    expect(doneList().length).toEqual(1);
+  });
+
+  it('returns a pending list', function(){
+    // hack to empty all
+    remove('a task');
+
+    add('a task');
+    expect(pendingList().length).toEqual(1);
+    doTask('a task');
+    expect(pendingList().length).toEqual(0);
+  });
+
+  it('returns all tasks', function(){
+    // hack to empty all
+    remove('a task');
+
+    add('a task1');
+    add('a task2');
+    add('a task3');
+    add('a task4');
+    expect(all().length).toEqual(4);
+
+  });
+
 });
