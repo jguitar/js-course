68aed1acdf5027ffae5fc244471a942258505e47
Juan F. Pérez <j.guitar@gmail.com>
2012-08-15 18:29:42 +0200

    unit2 refactoring

--- unit2/unit2.js
+++ unit2/unit2.js
@@ -1,13 +1,16 @@
 function FizzBuzz(n) {
-  if ((n % 3 == 0) && (n % 5 == 0)) {
-    return 'FizzBuzz'
-  } else if (n % 3 == 0) {
-    return 'Fizz';
-  } else if (n % 5 == 0) {
-    return 'Buzz';
-  } else {
-    return i + '';
-  }
+  if (isFizz(n) && isBuzz(n)) return 'FizzBuzz';
+  if (isFizz(n)) return 'Fizz';
+  if (isBuzz(n)) return 'Buzz';
+  return i + '';
+};
+
+function isFizz(n) {
+  return n % 3 == 0
+};
+
+function isBuzz(n) {
+  return n % 5 == 0
 };
 
 for(var i = 1; i <= 20; i++) {
