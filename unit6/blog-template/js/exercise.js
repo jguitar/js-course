c5298da64e2088f0358743ca3ed0bc5007c3e0a9
Juan F. Pérez <j.guitar@gmail.com>
2012-08-17 11:21:53 +0200

    unit 6, blog-template adding icon to h1

--- unit6/blog-template/js/exercise.js
+++ unit6/blog-template/js/exercise.js
@@ -1,14 +1,19 @@
 function addIcon() {
-	var content = document.getElementById('content');
-	
+  var content = document.getElementById('content');
+  var titles = content.getElementsByTagName('h1');
+  for (var i = 0; i < titles.length; i++){
+    icon = document.createElement('img');
+    icon.src = 'images/blog.png';
+    titles[i].insertBefore(icon);
+  }
 }
 
 function removeBlogArchive(){
-	var archive = document.getElementById('archive');
-	
+  var archive = document.getElementById('archive');
+
 }
 
 function addBlogRoll() {
 
-	var widget = document.createElement('div');
+  var widget = document.createElement('div');
 }
