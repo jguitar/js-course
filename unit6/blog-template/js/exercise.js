a496273ed039a17e295d380797022ac457a38edf
Juan F. Pérez <j.guitar@gmail.com>
2012-08-17 10:59:49 +0200

    unit 6, blog-template init

---
+++ unit6/blog-template/js/exercise.js
@@ -0,0 +1,14 @@
+function addIcon() {
+	var content = document.getElementById('content');
+	
+}
+
+function removeBlogArchive(){
+	var archive = document.getElementById('archive');
+	
+}
+
+function addBlogRoll() {
+
+	var widget = document.createElement('div');
+}
