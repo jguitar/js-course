f79929f5b01a17db17fa31477971c9b92faa17f9
Juan F. Pérez <j.guitar@gmail.com>
2012-08-23 21:21:25 +0200

    unit 6, blog-template adding blog roll, refactoring

--- unit6/blog-template/js/exercise.js
+++ unit6/blog-template/js/exercise.js
@@ -14,26 +14,41 @@ function removeBlogArchive(){
 }
 
 function addBlogRoll() {
+  var sidebar = document.getElementById('sidebar');
+  var widget = createWidget('Blog Roll', ['texto 1', 'algo mejor', 'un buen post']);
+  sidebar.appendChild(widget);
+}
+
+function createWidget(title, elements){
   var widget = document.createElement('div');
   widget.className = 'widget';
 
-  var h2 = document.createElement('h2');
-  h2.innerHTML = 'Blog roll';
+  var h2 = createH2(title);
+  widget.appendChild(h2);
+  var content = createContent(elements);
+  widget.appendChild(content);
+  return widget;
+}
 
+function createH2(title){
+  var h2 = document.createElement('h2');
+  h2.innerHTML = title;
+  return h2;
+}
+
+function createContent(elements){
   var content = document.createElement('div');
   content.className = 'contentarea';
 
   var list = document.createElement('ul');
-  for (var i = 0; i < 3; i++){
+  for (var i = 0; i < elements.length; i++){
+    var link = document.createElement('a');
+    link.href = '#';
+    link.innerHTML = elements[i];
     var li = document.createElement('li');
-    li.innerHTML = 'mi texto' + (i + 1);
+    li.appendChild(link);
     list.appendChild(li);
   }
   content.appendChild(list);
-
-  widget.appendChild(h2);
-  widget.appendChild(content);
-
-  var sidebar = document.getElementById('sidebar');
-  sidebar.appendChild(widget);
+  return content;
 }
