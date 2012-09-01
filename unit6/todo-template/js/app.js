bb82edfddcf736dfe9406b20d0113f864f514eec
Juan F. Pérez <j.guitar@gmail.com>
2012-09-01 11:58:16 +0200

    unit 6, todo-template init

---
+++ unit6/todo-template/js/app.js
@@ -0,0 +1,9 @@
+(function( window ) {
+	var taskList = new TaskList();
+
+	var newTask = document.getElementById('new-todo');
+
+	newTask.addEventListener('blur', function(event){
+	});
+
+})( window );
